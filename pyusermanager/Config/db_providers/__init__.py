d5318574757e06b706e34880200e20d74bc084c2
Aurvandill137 <cerberus885@gmail.com>
2022-03-17 16:16:08 +0100

    see Changelog Section in README.md

--- pyusermanager/Config/db_providers/__init__.py
+++ pyusermanager/Config/db_providers/__init__.py
@@ -1,6 +1,17 @@
+from enum import Enum
 from .prov_base import *
 from .prov_mysql import *
 from .prov_cockroachdb import *
 from .prov_oracle import *
 from .prov_postgresql import *
 from .prov_sqlite import *
+
+
+class DBProviders(Enum):
+
+    mysql = MYSQL_Provider
+    cockroachdb = CockroachDB_Provider
+    oracle = Oracle_Provider
+    postgresql = PostgreSQL_Provider
+    sqlite = SQLite_Provider
+    
